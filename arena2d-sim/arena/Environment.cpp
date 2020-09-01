c34b01ad437be5d9781d6a7ef343020ac2db4892
Marvyn Bornemann <m.bornemann@campus.tu-berlin.de>
2020-09-01 14:26:41 +0200

    deleted prints for nan-problem

--- arena2d-sim/arena/Environment.cpp
+++ arena2d-sim/arena/Environment.cpp
@@ -190,14 +190,9 @@ void Environment::getGoalDistance(float & l2, float & angle)
 {
 	if(_level != NULL && _level->getGoal() != NULL){
 		b2Vec2 goal_pos = _level->getGoalPosition();
-		b2Vec2 goal_pos_2 = _robot->getBody()->GetLocalPoint(goal_pos);
-		l2 = goal_pos_2.Length();
-		if(std::isnan(l2)){
-			INFO_F("\nERROR: Goal position: %.1f, %.1f\n", goal_pos.x, goal_pos.y);
-			INFO_F("\nERROR: Goal position 2: %.1f, %.1f\n", goal_pos_2.x, goal_pos_2.y);
-			INFO_F("\nERROR: Lenght l2: %.1f\n", l2);
-		}
-		angle = f_deg(zVector2D::signedAngle(zVector2D(0, 1), zVector2D(goal_pos_2.x, goal_pos_2.y)));
+		b2Vec2 goal_pos = _robot->getBody()->GetLocalPoint(goal_pos);
+		l2 = goal_pos.Length();
+		angle = f_deg(zVector2D::signedAngle(zVector2D(0, 1), zVector2D(goal_pos.x, goal_pos.y)));
 	}
 }
 
@@ -243,7 +238,6 @@ void Environment::BeginContact(b2Contact * contact){
 			}
 			else if(_level->checkHumanContact(other_fix)){
 				_reward += _SETTINGS->training.reward_human;
-				//INFO_F("\nHuman reward: %.3f\n", _SETTINGS->training.reward_human);
 				_episodeState = NEGATIVE_END;
 				_evaluation.countHuman();
 			}
