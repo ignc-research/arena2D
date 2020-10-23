31b4b2bd1ff55e971b798741cdfa63fafcee9ab8
Joe <noahinde2014@gmail.com>
2020-10-23 15:06:09 +0000

    solved compile errors

--- arena2d-sim/level/LevelFactory.hpp
+++ arena2d-sim/level/LevelFactory.hpp
@@ -9,7 +9,10 @@
 #include "Level.hpp"
 #include "LevelRandom.hpp"
 #include "LevelCustom.hpp"
+#ifdef USE_ROS
 #include "LevelStaticMap.hpp"
+#endif
+
 
 // singleton-get macro
 #define LEVEL_FACTORY LevelFactory::get()
@@ -82,12 +85,13 @@ public:
 		bool level_dynamic = params.getFlag("--dynamic");
 		return new LevelCustom(d, level_dynamic);
 	}
-
+	#ifdef USE_ROS
 	// static map level
 	Level* createLevelStaticMap(const LevelDef & d, const ConsoleParameters & params){
 		bool level_dynamic = params.getFlag("--dynamic");
 		return new LevelStaticMap(d,level_dynamic);
 	}
+	#endif
 
 private:
 
